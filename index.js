be4bcc4b3567429be0f638aeb6db95d903e2a882
Deepan Ghosh <107040689+dgdeepon@users.noreply.github.com>
2023-01-18 20:10:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server=require('json-server');
 const jsonServer=server.create();
 const route=server.router('db.json');
 const middle=server.defaults();
-const port= process.env.PORT() || 3000;
+const port= process.env.PORT || 3000;
 
 
 jsonServer.use(middle);
